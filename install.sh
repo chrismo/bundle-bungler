f314e6e6fcccc6f3392aab2109fc9e36797dd6b1
chrismo <chrismo@clabs.org>
2020-10-01 19:21:56 -0500

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -sf $(pwd)/rbset_local_bundle.rb ../rbset_local_bundle.rb
+ln -sf $(pwd)/set_local_bundle.sh ../set_local_bundle.sh
+ln -sf $(pwd)/unset_local_bundle.sh ../unset_local_bundle.sh
+
+chmod +x ../rbset_local_bundle.rb
+chmod +x ../set_local_bundle.sh
+chmod +x ../unset_local_bundle.sh
